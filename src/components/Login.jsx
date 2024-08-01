42022752826c271983eb1c6d6fc82814391d571e
Koteshwr-rao <koteswararaopakeeri@gmail.com>
2024-08-01 15:48:45 +0530

    sdfg

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"
+import { toast, ToastContainer } from "react-toastify";
 const Login = () => {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState({ id: '', password: '' });
-  const navigate = useNavigate()
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({ id: "", password: "" });
+  const navigate = useNavigate();
   const validate = () => {
     let valid = true;
-    const newErrors = { id: '', password: '' };
+    const newErrors = { id: "", password: "" };
 
     if (!id) {
-      newErrors.id = 'ID is required';
+      newErrors.id = "ID is required";
       valid = false;
     }
 
     if (!password) {
-      newErrors.password = 'Password is required';
+      newErrors.password = "Password is required";
       valid = false;
     }
 
@@ -28,41 +29,63 @@ const Login = () => {
     e.preventDefault();
     if (validate()) {
       // Submit form or perform login logic here
-      console.log('Form submitted with:', { id, password });
-      navigate("/dashboard")
+      console.log("Form submitted with:", { id, password });
+      const url = "http://localhost:5000/emp/login";
+      const creds = {
+        empId: id,
+        password: password,
+      };
+      axios
+        .post(url, creds)
+        .then((res) => {
+          navigate("/dashboard", { state: res.data });
+        })
+        .catch(err=>alert("Invalid Credentials"));
+
     }
   };
 
   return (
     <>
-      <div className='Payslip-SignUp'>
+      
+      <div className="Payslip-SignUp">
+        <ToastContainer/>
         <h2>Login</h2>
-        <form onSubmit={handleSubmit} className='credentials'>
+        <form onSubmit={handleSubmit} className="credentials">
           <div>
-            <input 
-              type="text" 
-              placeholder='ID' 
+            <input
+              type="text"
+              placeholder="ID"
               value={id}
-              onChange={(e) => setId(e.target.value)} 
+              onChange={(e) => setId(e.target.value)}
             />
-            {errors.id && <p style={{ color: 'red', margin: '0px 0px 0px 10px' }}>{errors.id}</p>}
+            {errors.id && (
+              <p style={{ color: "red", margin: "0px 0px 0px 10px" }}>
+                {errors.id}
+              </p>
+            )}
           </div>
           <div>
-            <input 
-              type="password" 
-              placeholder='Password' 
+            <input
+              type="password"
+              placeholder="Password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)} 
+              onChange={(e) => setPassword(e.target.value)}
             />
-            {errors.password && <p style={{ color: 'red', margin: '0px 0px 0px 10px' }}>{errors.password}</p>}
-            <a href="reset-password"><p style={{marginLeft:"10px "}}>Forgot Password?</p></a>
-
+            {errors.password && (
+              <p style={{ color: "red", margin: "0px 0px 0px 10px" }}>
+                {errors.password}
+              </p>
+            )}
+            <a href="reset-password">
+              <p style={{ marginLeft: "10px " }}>Forgot Password?</p>
+            </a>
           </div>
           <button type="submit">Login</button>
         </form>
       </div>
     </>
   );
-};  
+};
 
 export default Login;
