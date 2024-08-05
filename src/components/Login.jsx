b12619bf4f9eb646d34b4f51a8ac0bf6a1634ad6
PRAVALLIKA-2301 <123469975+PRAVALLIKA-2301@users.noreply.github.com>
2024-08-05 18:05:07 +0530

    hdh

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
-
 
 const Login = () => {
   const [id, setId] = useState("");
@@ -32,7 +31,7 @@ const Login = () => {
     if (validate()) {
       // Submit form or perform login logic here
       console.log("Form submitted with:", { id, password });
-      const url = "${process.env.REACT_APP_BACKEND_URL}/emp/login;"
+      const url = `${process.env.REACT_APP_BACKEND_URL}/emp/login`;
       const creds = {
         empId: id,
         password: password,
@@ -40,55 +39,59 @@ const Login = () => {
       axios
         .post(url, creds)
         .then((res) => {
-          console.log("the user data ",res.data[0]) 
+          console.log("the user data ", res.data[0]);
           navigate("/dashboard", { state: res.data[0] });
         })
-        .catch(err=>alert("Invalid Credentials"));
+        .catch((err) => alert("Invalid Credentials"));
     }
   };
 
   return (
     <>
       <div className="login-main">
-      <div className="Payslip-SignUp">
-        <ToastContainer/>
-        <div style={{display:"flex",alignItems:"center"}}>
-        <img style={{height:"40px",width:"40px"}} src="https://res.cloudinary.com/drqiw6wyl/image/upload/v1722853169/e1mki1ies2t0ttrypbra.jpg" alt="Logo" />
-        <h2 className="login-name">Login</h2>
+        <div className="Payslip-SignUp">
+          <ToastContainer />
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img
+              style={{ height: "40px", width: "40px" }}
+              src="https://res.cloudinary.com/drqiw6wyl/image/upload/v1722853169/e1mki1ies2t0ttrypbra.jpg"
+              alt="Logo"
+            />
+            <h2 className="login-name">Login</h2>
+          </div>
+          <form onSubmit={handleSubmit} className="credentials">
+            <div>
+              <input
+                type="text"
+                placeholder="ID"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+              />
+              {errors.id && (
+                <p style={{ color: "red", margin: "0px 0px 0px 10px" }}>
+                  {errors.id}
+                </p>
+              )}
+            </div>
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {errors.password && (
+                <p style={{ color: "red", margin: "0px 0px 0px 10px" }}>
+                  {errors.password}
+                </p>
+              )}
+              <a href="reset-password">
+                <p style={{ marginLeft: "20px " }}>Forgot Password?</p>
+              </a>
+            </div>
+            <button type="submit">Login</button>
+          </form>
         </div>
-        <form onSubmit={handleSubmit} className="credentials">
-          <div>
-            <input
-              type="text"
-              placeholder="ID"
-              value={id}
-              onChange={(e) => setId(e.target.value)}
-            />
-            {errors.id && (
-              <p style={{ color: "red", margin: "0px 0px 0px 10px" }}>
-                {errors.id}
-              </p>
-            )}
-          </div>
-          <div>
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {errors.password && (
-              <p style={{ color: "red", margin: "0px 0px 0px 10px" }}>
-                {errors.password}
-              </p>
-            )}
-            <a href="reset-password">
-              <p style={{ marginLeft: "20px " }}>Forgot Password?</p>
-            </a>
-          </div>
-          <button type="submit">Login</button>
-        </form>
-      </div>
       </div>
     </>
   );
