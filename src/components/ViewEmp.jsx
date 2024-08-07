193d69d28c27e04a08d4c33b16f81cef77d2e5fa
RaviKiran25052 <ravivarma25052@gmail.com>
2024-08-07 11:18:46 +0530

    we

--- src/components/ViewEmp.jsx
+++ src/components/ViewEmp.jsx
@@ -1,17 +1,46 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 
+
 const ViewEmp = () => {
-  const handleView = () =>{
-    const url = `http://localhost:5000/admin/ViewEmp`;
-    axios.post(url)
+  let dataList = []
+  const handleView = async() =>{
+    const url = `http://localhost:5000/admin/viewEmp`;
+    await axios.post(url)
     .then((res)=>{
-      console.log(res.data)
-    })
+      const data =res.data
+      for(var i = 0; i< data.length; i++){
+        dataList.push(data[i])
+    }
+  })
+  return dataList
   }
+  console.log(dataList)
+
+  useEffect(
+    handleView, [])
+
   return (
     <div className="viewCont">
-        
+      <table>
+        <thead>
+          <tr>
+          <th>S.No</th>
+          <th>Employee ID</th>
+          <th>Employee Name</th>
+          <th><button>Edit</button></th>
+          <th><button>Delete</button></th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataList.map((empDet)=>{
+            return(<tr key={empDet}>
+              <td>{empDet.empId}</td>
+            </tr>)
+          })
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
