f32a27a4cec1af77e7a1594d1e1b938fb4dbec5b
Koteshwr-rao <koteswararaopakeeri@gmail.com>
2024-08-09 15:47:51 +0530

    Dfg

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
 import "./App.css";
-import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import GeneratePayslips from "./components/GeneratePayslips";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import SignUp from "./components/Signup";
 import ViewEmp from "./components/ViewEmp";
+import Hero from "./components/Hero";
 function App() {
   const downloadPdf = async () => {
-    
-    GeneratePayslips({name:"KOTESWARA RAO PAKEERI",empId:"VTS2025040",grossSalary:86400,deducts:500,base:51000})
-  }
-  
+    GeneratePayslips({
+      name: "KOTESWARA RAO PAKEERI",
+      empId: "VTS2025040",
+      grossSalary: 86400,
+      deducts: 500,
+      base: 51000,
+    });
+  };
+
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/dashboard" element={<><Navbar />
-        <Hero /></>} />
-        <Route path="/" element={<Login />} />
-        <Route path="/reset-password" element={<SignUp />} />
-    </Routes>
-    </BrowserRouter>
-      
-      
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/dashboard"
+            element={
+              <>
+                {/* <Navbar /> */}
+                {/* <Hero /> */}
+              </>
+            }
+          />
+          <Route path="/" element={<Login />} />
+          <Route path="/reset-password" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+
       {/* <button onClick={downloadPdf}>Download payslip</button> */}
     </>
   );
